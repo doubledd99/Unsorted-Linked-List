355d426ec6ec634f86b4406467c8e1ec1c750d3c
doubledd99 <doubledd99@outlook.com>
2020-02-11 23:08:36 -0600

    deleted itemType

--- Unsorted Linked List/ItemType.cpp
+++
@@ -1,7 +0,0 @@
-#include "ItemType.h"
-
-
-ItemType::ItemType() {
-
-};
-//enum relationType { LESS, GREATER, EQUAL };
--- Unsorted Linked List/main.cpp
+++ Unsorted Linked List/main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-	UnsortedType unsortedList;
+	UnsortedType unsortedList;//creates new list
 	unsortedList.insertItem(420);	
 	unsortedList.insertItem(21);
 	unsortedList.insertItem(50);
 	unsortedList.insertItem(4920);
 	unsortedList.insertItem(69);
 	unsortedList.insertItem(1738);
-	unsortedList.insertItem(58008);
+	unsortedList.insertItem(58008); //adds items to list
 	unsortedList.insertItem(101);
 	unsortedList.insertItem(14);
 	unsortedList.insertItem(27);
@@ -21,26 +21,26 @@ int main() {
 	unsortedList.insertItem(3);
 	unsortedList.insertItem(2);
 	unsortedList.insertItem(1);
-	unsortedList.resetList();
+	unsortedList.resetList(); //resets position to head
 
 	UnsortedType list1;
-	UnsortedType list2;
+	UnsortedType list2; //creates new list
 	list1.insertItem(420);
 	list1.insertItem(69);
-	list1.insertItem(1738);
+	list1.insertItem(1738); //inserts items to list1
 	list1.insertItem(58008);
 	list1.insertItem(999);
 	list1.resetList();
 
 	list2.insertItem(11);
 	list2.insertItem(111);
-	list2.insertItem(1111);
+	list2.insertItem(1111); //inserts items to list2
 	list2.insertItem(11111);
 	list2.insertItem(111111);
 	list2.resetList();
 
 	/*for (int i = 0; i < 4; i++)
-		cout << list1.getNextItem() << endl; //will print items from list 1 for debugging
+		cout << list1.getNextItem() << endl;					//will print items from list 1 for debugging
 	cout << "\n \n \n \n";*/
 	/*for (int i = 0; i < 4; i++)
 		cout << list2.getNextItem() << endl;*/					//will pring items from list 2 for debugging
